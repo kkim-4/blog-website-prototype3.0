b2317df336e6daf3e58bed8609f1a99dd71ef9ea
kkim-4 <132178654+kkim-4@users.noreply.github.com>
2024-06-19 10:09:02 -0700

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar.jsx";
+import Blogs from "./Blogs.jsx";
+import Footer from "./Footer.jsx";
+import BlogContent from "./BlogContent.jsx"
+import SearchBar from "./Searchbar.jsx";
+
+export {
+    Navbar,
+    Blogs,
+    Footer,
+    BlogContent,
+    SearchBar
+}
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Homepage from "./Homepage.jsx";
+import BlogContentPage from "./BlogContentPage.jsx";
+
+export{ 
+    Homepage,
+    BlogContentPage
+}
